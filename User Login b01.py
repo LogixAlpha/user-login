c6d3f6f3ce379f4d31e34b28a092a68849b6261a
LogixAlpha <ayoub.ouaddi.yt@gmail.com>
2026-07-13 15:48:13 +0100

    Added time sleep

---
+++ User Login b01.py
@@ -0,0 +1,28 @@
+from colorama import Back , Fore , Style , init
+import time
+init(autoreset=True)
+
+print(f"{Back.LIGHTCYAN_EX} Welcome to Simple Login User! ".center(10))
+time.sleep(1)
+print(f"{Fore.LIGHTBLACK_EX} This project is Uncompleted! ".center(10))
+time.sleep(1.5)
+print(
+    f"1- {Fore.GREEN+Style.BRIGHT}Sign in {Style.RESET_ALL}\n"
+    f"2- {Fore.BLUE+Style.BRIGHT}Login in (soon) {Style.RESET_ALL}\n"
+)
+choice = input(f"{Back.LIGHTMAGENTA_EX+Style.DIM}Your Choice :{Style.RESET_ALL}")
+choice = int(choice)
+def script():
+    if choice == 1 :
+        username = input(f"{Back.LIGHTBLACK_EX+Style.BRIGHT}Enter your username:{Style.RESET_ALL}")
+        if len(username) <= 2 :
+            time.sleep(1)
+            print(f'{Style.BRIGHT+Fore.RED}3 characters or more required!{Style.RESET_ALL}')
+            time.sleep(0.5)
+            script()
+        else:
+            password = input(f"{Back.LIGHTBLACK_EX+Style.BRIGHT}Enter your password:{Style.RESET_ALL}")
+            if len(password) == 0 :
+                print(f"{Style.DIM}Welcome{Style.RESET_ALL} {Fore.MAGENTA+Style.BRIGHT}{username}{Style.RESET_ALL}! \n"
+                      f"{Fore.LIGHTRED_EX+Style.DIM}You didn't set any password!{Style.RESET_ALL} \n")    
+script()               
